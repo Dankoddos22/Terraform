4422e2708aeb6f111e0c87bdb9ea46e551fdd102
kali <kali@kali.kali>
2020-12-26 02:12:48 +0200

    rds

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "accesskey" {
+  default = "AKIAS4IGCXYIF47XB2ZV"
+}
+
+variable "secretkey" {
+  default = "KPWQV/wTYUipSdomxoTlB6eS0bs1RLaQRBPmyTH4"
+}
+
+variable "instance" {
+  default = "t2.micro"
+}
+
+variable "keypair" {
+  default = "LoadBalancer"
+}
